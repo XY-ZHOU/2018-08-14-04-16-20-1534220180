e03264a60580fa15a75b4dcfc88a75efa6bebd2f
XY-ZHOU <731986251@qq.com>
2018-09-24 20:52:36 +0800

    tongji

--- main/main.js
+++ main/main.js
@@ -1,6 +1,9 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
+    o) 最大值 = ${sequence.maxmum()}
+    o) 元素数量 = ${sequence.lenmum()}
+    o) 平均值 = ${sequence.avemum()}
 // Write your code here
 `);
 };
@@ -8,11 +11,38 @@ module.exports = function main() {
 class Sequence {
   constructor(input) {
     // Write your code here
+    this.input=input;
   }
-
-  minimum() {
+  var arr=[];
+  for(var i=0;i<this.input.length;i++){
+    for(var j=i+1;j<this.input.length;j++){
+      if(this.input[i]>this.input[j]){
+          var temp1=this.input[i];
+          this.input[i]=this.input[j];
+          this.input[j]=temp;
+      }
+    }
+  }
+  var arrlen=arr.length;
+  minimum(){
     // Write your code here
+    return arr[0];
+
+  }
+  // Write your code here
+  maxmum(){
+      return arr[arrlen-1];
+  }
+  lenmum(){
+      return arrlen;
+  }
+  avemum(){
+      var sum=0;
+      for(var k=0;k<arrlen.length;k++){
+        sum+=arr[k];
+      }
+      var ave=(sum/arrlen).toFixed(2);
+      return (ave);
   }
 
-  // Write your code here
 }
